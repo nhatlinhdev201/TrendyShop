893e835f542e713b3133d5ea0b13215f2b666a6d
quyentrann <dangthiquyentran22@gmail.com>
2023-10-24 14:08:28 +0700

    Quyen Tran update : more action

--- src/constance/SetBountJPanel.java
+++ src/constance/SetBountJPanel.java
@@ -3,6 +3,6 @@ package constance;
 public class SetBountJPanel {
 	public static final int X = 0;
     public static final int Y = 0;
-    public static final int WIDTH = 1712;
-    public static final int HEIGHT = 881;
+    public static final int WIDTH = 1270;
+    public static final int HEIGHT = 630;
 }
