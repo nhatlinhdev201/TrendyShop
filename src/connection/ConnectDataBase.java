5d1ac545e6c72d57d80150083678bc5271909c69
quyentrann <dangthiquyentran22@gmail.com>
2023-11-12 14:19:08 +0700

    fix add connect

---
+++ src/connection/ConnectDataBase.java
@@ -0,0 +1,28 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDataBase {
+	public Connection connection;
+	public static ConnectDataBase instance;
+	public ConnectDataBase() {
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=trendyshop","sa","123");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static ConnectDataBase getInstance() {
+		if (instance == null) {
+			instance = new ConnectDataBase();
+		}
+		return instance;
+	}
+	public Connection getConnection() {
+		return connection;
+	}
+	
+}
